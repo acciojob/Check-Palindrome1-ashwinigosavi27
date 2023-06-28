a728f3a6c720f90735ed9ef785bd88f5ac9b5eff
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-06-28 21:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+str = str.toLowerCase();
+	str = str.trim();
+	let i = 0, j = str.length-1;
+	let isPalindrome = true;
+	while(i <= j){
+		if(str.charAt(i)!= str.charAt(j)){
+			isPalindrome = false;
+			return isPalindrome;
+		}
+		i++;
+		j--;
+	}
+	return isPalindrome;
 }
 module.exports = palindrome
